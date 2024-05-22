4d8196992f83fc86f0a605dc71e691e720d1dacb
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-05-22 11:15:07 +0530

    Fixed relative path

--- assignment/src/components/CourseCard/CourseCard.jsx
+++ assignment/src/components/CourseCard/CourseCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const CourseCard = (course) => {
   return (
-    <Link to={`course/${course.course._id}`}>
+    <Link to={`/course/${course.course._id}`}>
       <div className="course_card_container">
         <img className="course_image" src={course.course.coverImage} alt="" />
         <h4>{course.course.title}</h4>
