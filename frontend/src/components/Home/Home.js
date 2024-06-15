46fe579d45ea2bca28d013350a43ddbe51327074
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-06-15 20:57:30 +0530

    renaming frontend folder

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import CourseCard from "../CourseCard/CourseCard";
+import Explore from "../Explore/Explore";
+import MyCourse from "../MyCourse/MyCourse";
+import { useAuth } from "../../context/AuthContext";
+
+const Home = () => {
+  const { user } = useAuth();
+  return (
+    <div className="home_container">
+      <div className="home_banner">
+        <h1>iGOT Learning platform</h1>
+      </div>
+      {user && <MyCourse isHomeComponent={true} />}
+      <Explore isHomeComponent={true} />
+    </div>
+  );
+};
+
+export default Home;
