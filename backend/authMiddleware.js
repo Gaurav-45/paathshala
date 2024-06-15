dab79d81c4ca0d75eca2d338c1d668e65cf72a70
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-06-15 20:59:39 +0530

    Auth middleware fixed

--- backend/authMiddleware.js
+++ backend/authMiddleware.js
@@ -14,13 +14,16 @@ const protect = async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      console.log("Token:", token); // Log the token
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log("Decoded Token:", decoded); // Log the decoded token
 
       req.user = await User.findById(decoded.id).select("-password");
-      console.log("Authenticated User:", req.user); // Log the authenticated user
+
+      if (!req.user) {
+        return res
+          .status(401)
+          .json({ message: "Not authorized, user not found" });
+      }
 
       next();
     } catch (error) {
@@ -31,5 +34,28 @@ const protect = async (req, res, next) => {
     res.status(401).json({ message: "Not authorized, no token" });
   }
 };
+
+// const protect = async (req, res, next) => {
+//   let token;
+
+//   if (
+//     req.headers.authorization &&
+//     req.headers.authorization.startsWith("Bearer")
+//   ) {
+//     try {
+//       token = req.headers.authorization.split(" ")[1];
+//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//       req.user = await User.findById(decoded.id).select("-password");
+//       next();
+//     } catch (error) {
+//       console.error(error);
+//       res.status(401).json({ message: "Not authorized, token failed" });
+//     }
+//   }
+
+//   if (!token) {
+//     res.status(401).json({ message: "Not authorized, no token" });
+//   }
+// };
 
 module.exports = { generateToken, protect };
