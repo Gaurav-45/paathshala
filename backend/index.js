8a2f2da030ee63755060347ac7dcf6050388ca16
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-06-22 21:00:39 +0530

    Added cors config

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,33 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const mongoString = process.env.DATABASE_URL;
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-// app.use(bodyParser.json());
 const routes = require("./routes/routes");
 const paymentRoutes = require("./routes/payment");
 
+const app = express();
+app.use(express.json());
+// app.use(cors());
+const allowedOrigins = [
+  "https://igot-coursera-noem.vercel.app/",
+  "http://localhost:3000",
+];
+
+// Configure CORS
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// app.use(bodyParser.json());
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
