8490dd2b6b7bfb2e2665de344ebe5e08d38243d6
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-06-22 23:03:37 +0530

    Updating cors

--- backend/index.js
+++ backend/index.js
@@ -9,26 +9,26 @@ const paymentRoutes = require("./routes/payment");
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
-const allowedOrigins = [
-  "https://igot-coursera-noem.vercel.app/",
-  "http://localhost:3000",
-];
+app.use(cors());
+// const allowedOrigins = [
+//   " ",
+//   "http://localhost:3000",
+// ];
 
-// Configure CORS
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// // Configure CORS
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
 // app.use(bodyParser.json());
 mongoose.connect(mongoString);
