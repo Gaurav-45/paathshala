cfa871f87dcff4634fbe867824ea510f4e181a01
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-06-23 12:32:27 +0530

    adding options in cors all

--- backend/index.js
+++ backend/index.js
@@ -29,8 +29,7 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect(mongoString);
 const database = mongoose.connection;
